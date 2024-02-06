f799dfa8ceba13a56823de5f863fd7ff1a3e7ba1
Ferenc Zachar <ferenczachar@gmail.com>
2024-02-06 21:22:35 +0100

    Deleted unused files and created search functionality with useContext

--- client/src/components/PostsContainer.jsx
+++ client/src/components/PostsContainer.jsx
@@ -7,6 +7,7 @@ import { QueryContext } from '../QueryContext'
 export default function PostsContainer(){
     const { queryValue } = useContext(QueryContext)
     const [posts, setPosts] = useState([]);
+    let filterForPosts = false;
 
     const fetchPosts = async () => {
         try {
@@ -26,16 +27,44 @@ export default function PostsContainer(){
             console.error('Error in catch:', error);
         }
     }
+    
 
     useEffect(() => {
         fetchPosts();
         // eslint-disable-next-line
     }, [queryValue])
 
+    if (posts && queryValue !== '' && queryValue.length > 3) { //Ha vannak posztok
+        filterForPosts = true;
+    }
+
+    let filteredPosts;
+
+    if (filterForPosts) {
+        filteredPosts = posts.filter((post) => {
+            return post.title.toLowerCase().includes(queryValue.toLowerCase()) || post.desc.toLowerCase().includes(queryValue.toLowerCase())
+        });
+    }
+
+
     return (
         <div className='postsContainerFull'>
             <div className="postsContainer">
-                {posts.map((post) => (
+                {filterForPosts &&
+                 filteredPosts?.map((post) => (
+                    <Post
+                        key={post.id}
+                        creator={post.username}
+                        title={post.title}
+                        desc={post.desc}
+                        likes={post.likes}
+                        createdAt={post.createdAt}
+                        img={post.img}
+                        postId={post.id}
+                    />
+                ))}
+                {!filterForPosts && 
+                posts?.map((post) => (
                         <Post
                             key={post.id}
                             creator={post.username}
